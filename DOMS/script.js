98d0a9e30b3d3b62ed0ee406e099e586c381b63c
ajujawade <ajayjawade06@gmail.com>
2024-03-11 23:39:59 +0530

    added

---
+++ DOMS/script.js
@@ -0,0 +1,20 @@
+let q1 = document.querySelector('.two')
+console.log(q1)
+
+let q2 = document.querySelector('.cc')
+console.log(q2)
+
+let q3 = document.querySelector('h1')
+console.log(q3)
+
+let byttribute = document.querySelector(id = '[one]')
+
+let button = document.querySelector('button')
+let text = document.querySelector
+
+
+button.addEventListener('click', function() {
+
+    let textcolor = inp
+
+})
---
+++ lambda.py
@@ -0,0 +1,54 @@
+# lambda function
+
+add = lambda x,y:x+y
+
+result = add(12,8)
+print(result)
+
+
+# square
+
+square = lambda x:x**2
+
+result = square(21)
+print(result)
+
+# function as parameter to another function
+
+add = lambda x,y : x+y
+
+def addition(fn,x,y):
+    result = fn(x,y)
+    return result
+
+res = addition(add,34,6)
+print(res)
+
+sub = lambda x,y:x-y
+
+def substraction(fn,x,y):
+    result = fn(x,y)
+    return(result)
+    
+res = substraction(sub,20,8)
+
+print(res)
+
+# Program 4
+# Function as a return type
+
+def add():
+    return lambda x,y:x+y
+
+add = add()
+
+result = add(35,9)
+
+print(result)
+
+
+
+    
+
+
+
