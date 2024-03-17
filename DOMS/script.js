2f8fb89115a293dbc82fe64c54e3699ff73df997
ajujawade <ajayjawade06@gmail.com>
2024-03-18 00:04:59 +0530

    added

--- DOMS/script.js
+++ DOMS/script.js
@@ -63,13 +63,12 @@ let headone = document.querySelector('h1')
 
 changecolorbutton.addEventListener('click', function() {
 
-    let val = intext.value
-    headone.style.color = val
-    ulist.style.color = val
-    intext.value = ''
+    let tex = intext.value
+    headone.style.color = tex
+    ulist.style.color = tex
+    intext.value = ""
 
 })
-
 
 
 addelbutton.addEventListener('click', function() {
@@ -77,26 +76,43 @@ addelbutton.addEventListener('click', function() {
     let newel = document.createElement('li')
     let text = intext.value
     newel.textContent = text
-    if (text == '') {
+    if (intext.value == "") {
         pass
     } else {
-        createButton('Remove', 'remove', newel)
-        createButton('Up', 'up', newel)
-        createButton('Down', 'down', newel)
+        newButton('Remove', 'remove', newel)
+        newButton('Up', 'up', newel)
+        newButton('Down', 'down', newel)
     }
     ulist.appendChild(newel)
-    intext.value = ''
+    intext.value = ""
 
 })
 
+// function newButton(li) {
 
+//     let r = document.createElement('button')
+//     r.textContent = "Remove"
+//     r.classList.add('remove')
+//     li.appendChild(r)
 
-function createButton(textCon, className, el) {
+//     let u = document.createElement('button')
+//     u.textContent = "Up"
+//     u.classList.add('up')
+//     li.appendChild(u)
+
+//     let d = document.createElement('button')
+//     d.textContent = "Down"
+//     d.classList.add('down')
+//     li.appendChild(d)
+
+// }
+
+function newButton(textcon, classname, li) {
 
     let newbutton = document.createElement('button')
-    newbutton.textContent = textCon
-    newbutton.classList.add(className)
-    el.appendChild(newbutton)
+    newbutton.textContent = textcon
+    newbutton.classList.add(classname)
+    li.appendChild(newbutton)
 
 
 }
@@ -106,34 +122,38 @@ ulist.addEventListener('click', function(event) {
     // console.log(event)
     // console.log(event.target)
     // console.log(event.target.tagName)
+    // console.log(event.target.className)
 
-    if (event.target.tagName === 'BUTTON') {
+    if (event.target.tagName === "BUTTON") {
 
-        if (event.target.className === 'remove') {
-
-            let li = event.target.parentElement
-            let ulist = li.parentElement
-            ulist.removeChild(li)
-
-        } else if (event.target.className === 'up') {
+        if (event.target.className === "remove") {
 
             let li = event.target.parentElement
-            let ulist = li.parentElement
+            let ul = li.parentElement
+            ul.removeChild(li)
+
+        }
+
+        if (event.target.className === "up") {
+
+            let li = event.target.parentElement
+            let ul = li.parentElement
             let prev = li.previousElementSibling
             if (prev) {
 
-                ulist.insertBefore(li, prev)
+                ul.insertBefore(li, prev)
 
             }
 
-        } else if (event.target.className === 'down') {
+        }
 
+        if (event.target.className === "down") {
             let li = event.target.parentElement
-            let ulist = li.parentElement
+            let ul = li.parentElement
             let next = li.nextElementSibling
             if (next) {
 
-                ulist.insertBefore(next, li)
+                ul.insertBefore(next, li)
 
             }
 
@@ -142,3 +162,93 @@ ulist.addEventListener('click', function(event) {
 
 
 })
+
+
+
+
+
+
+
+
+
+// changecolorbutton.addEventListener('click', function() {
+
+//     let val = intext.value
+//     headone.style.color = val
+//     ulist.style.color = val
+//     intext.value = ''
+
+// })
+
+
+
+// addelbutton.addEventListener('click', function() {
+
+//     let newel = document.createElement('li')
+//     let text = intext.value
+//     newel.textContent = text
+//     if (text == '') {
+//         pass
+//     } else {
+//         createButton('Remove', 'remove', newel)
+//         createButton('Up', 'up', newel)
+//         createButton('Down', 'down', newel)
+//     }
+//     ulist.appendChild(newel)
+//     intext.value = ''
+
+// })
+
+
+
+// function createButton(textCon, className, el) {
+
+//     let newbutton = document.createElement('button')
+//     newbutton.textContent = textCon
+//     newbutton.classList.add(className)
+//     el.appendChild(newbutton)
+
+
+// }
+
+// ulist.addEventListener('click', function(event) {
+
+//     // console.log(event)
+//     // console.log(event.target)
+//     // console.log(event.target.tagName)
+
+//     if (event.target.tagName === 'BUTTON') {
+
+//         if (event.target.className === 'remove') {
+
+//             let li = event.target.parentElement
+//             let ulist = li.parentElement
+//             ulist.removeChild(li)
+
+//         } else if (event.target.className === 'up') {
+
+//             let li = event.target.parentElement
+//             let ulist = li.parentElement
+//             let prev = li.previousElementSibling
+//             if (prev) {
+
+//                 ulist.insertBefore(li, prev)
+
+//             }
+
+//         } else if (event.target.className === 'down') {
+
+//             let li = event.target.parentElement
+//             let ulist = li.parentElement
+//             let next = li.nextElementSibling
+//             if (next) {
+
+//                 ulist.insertBefore(next, li)
+
+//             }
+
+//         }
+//     }
+
+
+// })
